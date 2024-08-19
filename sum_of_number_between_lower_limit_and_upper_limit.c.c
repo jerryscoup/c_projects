aa5406634f5a24ca48ec9f6e79a1115de34a0ebd
prince/jerry_scoup <workforpuskarpuneet@gmail.com>
2024-08-19 23:54:58 +0530

    Update sum_of_number_between_lower_limit_and_upper_limit.c.c

--- sum_of_number_between_lower_limit_and_upper_limit.c.c
+++ sum_of_number_between_lower_limit_and_upper_limit.c.c
@@ -1,34 +1,6 @@
 #include <stdio.h>
 
 void main() {
-    int number, sum = 0, digit;
-
-    
-    printf("Enter the number: ");
-    scanf("%d", &number);
-
-    
-    while (number > 0) {
-        
-        digit = number % 10;
-        
-        sum += digit;
-        
-        number /= 10;
-    }
-
-    
-    printf("Sum of digits: %d\n", sum);
-
-    
-}
-
-
-
-
-#include <stdio.h>
-
-void main() {
     int lower_limit, upper_limit, sum = 0, i;
 
     
