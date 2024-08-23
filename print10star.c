5fdad4cec9569f4932cf08e78aebca17493ecd8f
prince/jerry_scoup <workforpuskarpuneet@gmail.com>
2024-08-23 18:17:26 +0530

    Add files via upload

---
+++ print10star.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    
+    // Print 10 stars in a row
+    for(i = 0; i < 10; i++) {
+        printf("*");
+    }
+    
+    printf("\n"); // Move to the next line after printing the stars
+    
+    return 0;
+}
