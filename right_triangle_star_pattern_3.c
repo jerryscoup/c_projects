4a023e049017992cef7e3e7b87cfef56c858b56b
prince/jerry_scoup <workforpuskarpuneet@gmail.com>
2024-08-25 11:34:12 +0530

    Add files via upload

---
+++ right_triangle_star_pattern_3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main() {
+    int rows, i, j;
+
+    
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    
+    for (i = 1; i <= rows; i++) {
+        
+        for (j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        
+        printf("\n");
+    }
+
+    
+}
