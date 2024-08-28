cbe85494535dc15913ce638f79fe53c6f0f0a050
prince/jerry_scoup <workforpuskarpuneet@gmail.com>
2024-08-28 21:03:45 +0530

    Add files via upload

---
+++ float.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+
+    int a;
+    float b;
+    printf("enter the number a:");
+    scanf("%d",&a);
+    printf("enter the number b:");
+    scanf("%f",&b);
+
+    printf("int and float is %d,%f\n",a,b);
+}
