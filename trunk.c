bb492c080827281fe1500ec4995e6b1202ea5926
prince/jerry_scoup <workforpuskarpuneet@gmail.com>
2024-08-29 21:17:53 +0530

    Add files via upload

---
+++ trunk.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+
+    int a,b;
+    printf("enter the number a:");
+    scanf("%d",&a);
+    printf("enter the number b:");
+    scanf("%d",&b);
+
+    printf("the sum of a: %d and b: %d is %d",a,b,a+b);
+}
