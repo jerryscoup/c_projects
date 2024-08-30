8e94047c993d02a3e44b0bd5fc42f51324395417
prince/jerry_scoup <workforpuskarpuneet@gmail.com>
2024-08-30 12:11:00 +0530

    Add files via upload

---
+++ mul.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+
+    int a,b;
+    printf("enter the number a:");
+    scanf("%d",&a);
+    printf("enter the number b:");
+    scanf("%d",&b);
+
+    printf("the product of a: %d and b: %d is %d\n",a,b,a*b);
+}
