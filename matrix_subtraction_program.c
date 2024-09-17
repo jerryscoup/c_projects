54d129f3ee453b287dd18c338b3b2b7b4f4094ef
prince/jerry_scoup <workforpuskarpuneet@gmail.com>
2024-09-17 16:36:19 +0530

    Add files via upload

---
+++ matrix_subtraction_program.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+#define MAX_ROWS 100
+#define MAX_COLS 100
+
+int main() {
+    int matrix1[MAX_ROWS][MAX_COLS], matrix2[MAX_ROWS][MAX_COLS], difference[MAX_ROWS][MAX_COLS];
+    int rows, cols;
+
+   
+    printf("Enter the number of rows and columns for the matrices: ");
+    scanf("%d %d", &rows, &cols);
+
+    
+    printf("Enter elements of the first matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i+1, j+1);
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    
+    printf("Enter elements of the second matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i+1, j+1);
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+   
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            difference[i][j] = matrix1[i][j] - matrix2[i][j];
+        }
+    }
+
+    
+    printf("Resultant matrix after subtraction:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", difference[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
