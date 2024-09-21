fb5e8869a96d02349dfa47ab5c266a073bb73d96
prince/jerry_scoup <workforpuskarpuneet@gmail.com>
2024-09-21 17:25:58 +0530

    area_and_perimeter_of_square_and_rectangle.c

---
+++ area_and_perimeter_of_square_and_rectangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int side,length,breadth;
+printf("enter the side measurement for square");
+scanf("%d",&side);
+printf("the area and perimeter of square is %d & %d",side*side,4*side);
+printf("please enter the values of length and breadth of rectangle");
+scanf("%d %d",&length,&breadth);
+printf("the area and perimeter of the respective rectangle is %d and %d",length*breadth,2*(length+breadth));
+  
+
+
+
+}
