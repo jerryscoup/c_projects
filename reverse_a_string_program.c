1415a196aca976529be7a7b1d4b42a6a49777ab1
prince/jerry_scoup <workforpuskarpuneet@gmail.com>
2024-09-26 21:15:52 +0530

    Add files via upload

---
+++ reverse_a_string_program.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+
+void reverseString(char *str) {
+    int length = strlen(str);
+    for (int i = 0; i < length / 2; i++) {
+        char temp = str[i];
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = temp;
+    }
+}
+
+int main() {
+    char str[100];
+
+   
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    
+    if (str[strlen(str) - 1] == '\n') {
+        str[strlen(str) - 1] = '\0';
+    }
+
+   
+    reverseString(str);
+
+    
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
+
